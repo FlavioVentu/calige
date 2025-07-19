2d0488a03cece10604b15464c614d7524e69b91f
FlavioVentu <s5667103@studenti.unige.it>
2025-07-19 17:49:52 +0200

    Rename all class file with uppercase on the first letter, show profile complete

---
+++ public_html/backend/db/queries/Stmt.php
@@ -0,0 +1,33 @@
+<?php
+
+# classe astratta per tutte le query
+abstract class Stmt {
+
+    private $con;
+    private $query;
+
+    public function __construct(mysqli $con, string $query) {
+        $this->con = $con;
+        $this->query = $query;
+    }
+
+    # funzione astratta da implementare in ogni classe che la estende
+    abstract public function execute();
+
+
+    # parte iniziale di un prepared stmt
+    protected function init() {
+
+         # se la query non va a buon fine lancio un'eccezione
+        if (!($res = $this->con->query($this->query))) {
+            throw new mysqli_sql_exception("Errore nell'esecuzione della query" . get_class($this));
+        }
+
+        return $res;
+    }
+
+    # metodo per la chiusura del risultato
+    protected function close(mysqli_result $res) {
+        $res->free();
+    }
+}
---
+++ public_html/backend/db/queries/UserShowProfile.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'Stmt.php';
+
+class UserShowProfile extends Stmt {
+
+    public function execute() {
+        $res = $this->init();
+
+        if($res->num_rows !== 1) {
+            throw new mysqli_sql_exception("L'utente non è stato trovato:" . get_class($this));
+        }
+
+        if(!($row = $res->fetch_assoc())) {
+            throw new mysqli_sql_exception("Errore nella restituzione del record nella query:" . get_class($this));
+        }
+
+        $this->close($res);
+
+        return $row;
+    }
+}
